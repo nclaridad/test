ec950e31a60daf122a2a3c466321a85c4858744a
angelochiu <achiu@emis.com>
2017-03-23 09:33:57 +0800

    JSON

--- process.php
+++ process.php
@@ -1,8 +1,8 @@
 <?php
-
+// E_ERROR(1);
 $dbhost = "localhost";
-$dbuser = "nclaridad";
-$dbpass = "root";
+$dbuser = "root";
+$dbpass = "isiachiu";
 $dbname = "ajax";
 	
 //Connect to MySQL Server
@@ -39,24 +39,30 @@ $query = "SELECT * FROM ajax_example WHERE gender = '$gender'";
 $qry_result = mysqli_query($con, $query) or die(mysqli_error($con));
 
 //Build Result String
-$display_string = "<table>";
-$display_string .= "<tr>";
-$display_string .= "<th>First Name</th>";
-$display_string .= "<th>Last Name</th>";
-$display_string .= "<th>Gender</th>";
-$display_string .= "</tr>";
-
+// $display_string = "<table>";
+// $display_string .= "<tr>";
+// $display_string .= "<th>First Name</th>";
+// $display_string .= "<th>Last Name</th>";
+// $display_string .= "<th>Gender</th>";
+// $display_string .= "</tr>";
+$display_string = '';
 // Insert a new row in the table for each person returned
+$arr = array();
 while($row = mysqli_fetch_array($qry_result)){
-   $display_string .= "<tr>";
-   $display_string .= "<td>". $row['firstName'] . "</td>";
-   $display_string .= "<td>". $row['lastName'] . "</td>";
-   $display_string .= "<td>". $row['gender'] . "</td>";
-   $display_string .= "</tr>";
+   // $display_string .= "<tr>";
+   // $display_string .= "<td>". $row['firstName'] . "</td>";
+   // $display_string .= "<td>". $row['lastName'] . "</td>";
+   // $display_string .= "<td>". $row['gender'] . "</td>";
+   // $display_string .= "</tr>";
+   $arr[] = array(
+         'firstName' => $row['firstName'],
+         'lastName'  => $row['lastName'],
+         'gender'    => $row['gender'],
+      );
 }
 
-echo "Query: " . $query . "<br />";
-$display_string .= "</table>";
-
-echo $display_string;
+//echo "Query: " . $query . "<br />";
+//$display_string .= "</table>";
+echo json_encode($arr); //Convert array to JSON(Object Notation)
+//echo $display_string;
 ?>
